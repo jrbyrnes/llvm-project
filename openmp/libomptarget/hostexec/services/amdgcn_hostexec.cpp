059db9732a0c4f363caaea25ce6099649395e5af
Ron Lieberman <ron.lieberman@amd.com>
2024-09-10 17:10:49 -0500

    SWDEV-484055 Fixes aborts in CPX mode
    AMD_MAX_HSA_AGENTS=16 is too small for CPX which is 64
    This patch moves to 256.
    
    Change-Id: Ibcf51bc060a071698b439ba2486a1635765955f0

--- openmp/libomptarget/hostexec/services/amdgcn_hostexec.cpp
+++ openmp/libomptarget/hostexec/services/amdgcn_hostexec.cpp
@@ -26,7 +26,7 @@
 #include <thread>
 
 /// Defines how many GPUs are maximally supported on a system
-#define AMD_MAX_HSA_AGENTS 16
+#define AMD_MAX_HSA_AGENTS 256
 
 /** Opaque wrapper for signal */
 typedef struct {
