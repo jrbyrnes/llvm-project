debdb14b601326c1f4c8e84a784f7ee1427a47e0
Jeffrey Byrnes <Jeffrey.Byrnes@amd.com>
2025-07-01 16:11:40 -0700

    [AMDGPU] Add option to preinflate to AVGPR
    Change-Id: Ia488b12f06bdc3e462f1cd90baf64a3375f15c4c

--- llvm/lib/Target/AMDGPU/GCNPreRAOptimizations.cpp
+++ llvm/lib/Target/AMDGPU/GCNPreRAOptimizations.cpp
@@ -35,6 +35,11 @@ using namespace llvm;
 
 #define DEBUG_TYPE "amdgpu-pre-ra-optimizations"
 
+static cl::opt<bool>
+    InflateToAVGPR("amdgpu-avgpr-inflation", cl::Hidden, cl::init(false),
+                   cl::desc("Enable register inflation to avgpr register class "
+                            "(which can be assigned to either AGPR or VGPR)."));
+
 namespace {
 
 class GCNPreRAOptimizations : public MachineFunctionPass {
@@ -229,6 +234,13 @@ bool GCNPreRAOptimizations::runOnMachineFunction(MachineFunction &MF) {
     if (!LIS->hasInterval(Reg))
       continue;
     const TargetRegisterClass *RC = MRI->getRegClass(Reg);
+
+    if (InflateToAVGPR && ST.hasGFX90AInsts() &&
+        (TRI->isAGPRClass(RC) || TRI->isVGPRClass(RC))) {
+      MRI->recomputeRegClass(Reg);
+      continue;
+    }
+
     if ((RC->MC->getSizeInBits() != 64 || !TRI->isSGPRClass(RC)) &&
         (ST.hasGFX90AInsts() || !TRI->isAGPRClass(RC)))
       continue;
