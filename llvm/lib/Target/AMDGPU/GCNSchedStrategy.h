9cef89ca35ada6e4c3471cd7f3bc8dd7eeabaa2e
Jeffrey Byrnes <Jeffrey.Byrnes@amd.com>
2025-07-24 12:30:25 -0700

    Move into GCNPreRAOptimizations
    Change-Id: I0e0e8acbb5e02713d9e3497e3474be8be244b1f9

--- llvm/lib/Target/AMDGPU/GCNSchedStrategy.h
+++ llvm/lib/Target/AMDGPU/GCNSchedStrategy.h
@@ -28,12 +28,11 @@ class GCNSchedStage;
 
 enum class GCNSchedStageID : unsigned {
   OccInitialSchedule = 0,
-  AVGPRRewriteSchedule = 1,
-  UnclusteredHighRPReschedule = 2,
-  ClusteredLowOccupancyReschedule = 3,
-  PreRARematerialize = 4,
-  ILPInitialSchedule = 5,
-  MemoryClauseInitialSchedule = 6
+  UnclusteredHighRPReschedule = 1,
+  ClusteredLowOccupancyReschedule = 2,
+  PreRARematerialize = 3,
+  ILPInitialSchedule = 4,
+  MemoryClauseInitialSchedule = 5
 };
 
 #ifndef NDEBUG
@@ -225,7 +224,6 @@ using RegionBoundaries =
 class GCNScheduleDAGMILive final : public ScheduleDAGMILive {
   friend class GCNSchedStage;
   friend class OccInitialScheduleStage;
-  friend class AVGPRRewriteScheduleStage;
   friend class UnclusteredHighRPStage;
   friend class ClusteredLowOccStage;
   friend class PreRARematStage;
@@ -257,9 +255,6 @@ class GCNScheduleDAGMILive final : public ScheduleDAGMILive {
 
   // Regions that has the same occupancy as the latest MinOccupancy
   BitVector RegionsWithMinOcc;
-
-  // Regions which use the AV RC.
-  BitVector RegionsWithAVRegs;
 
   // Regions that have IGLP instructions (SCHED_GROUP_BARRIER or IGLP_OPT).
   BitVector RegionsWithIGLPInstrs;
@@ -406,18 +401,6 @@ public:
   bool shouldRevertScheduling(unsigned WavesAfter) override;
 
   OccInitialScheduleStage(GCNSchedStageID StageID, GCNScheduleDAGMILive &DAG)
-      : GCNSchedStage(StageID, DAG) {}
-};
-
-class AVGPRRewriteScheduleStage : public GCNSchedStage {
-private:
-  bool reconstrainRegClass(Register Reg,
-                           const TargetRegisterClass *NewRC) const;
-
-public:
-  bool initGCNSchedStage() override;
-
-  AVGPRRewriteScheduleStage(GCNSchedStageID StageID, GCNScheduleDAGMILive &DAG)
       : GCNSchedStage(StageID, DAG) {}
 };
 
