ccaf2441be2e29e222127481ae96fdaac834a71a
Jeffrey Byrnes <Jeffrey.Byrnes@amd.com>
2025-08-18 15:38:14 -0700

    Get it to work
    Change-Id: Ic580774dd4d09bc8ceccc7afad32740c66eb2a62

--- llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
+++ llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
@@ -2271,13 +2271,15 @@ bool PreRARematStage::initGCNSchedStage() {
     }
   }
 
+
   return true;
 }
 
 
 bool PreRARematStage::initGCNRegion() {
-  //if (!DAG.RescheduleRegions[RegionIdx])
-  //  return false;
+  return false;
+  if (!DAG.RescheduleRegions[RegionIdx])
+    return false;
 
   return GCNSchedStage::initGCNRegion();
 }
