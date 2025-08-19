5748fa41fc0fce5bb3e34f22183b8cbc747fabaa
Jeffrey Byrnes <Jeffrey.Byrnes@amd.com>
2025-08-19 15:54:47 -0700

    Fix issues
    Change-Id: I8aa9a59a389cd536e10bf5627936ed614d7764f2

--- llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
+++ llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
@@ -1146,6 +1146,13 @@ bool RewriteScheduleStage::initGCNSchedStage() {
   unsigned ArchVGPRThreshold =
       ST.getMaxNumVectorRegs(DAG.MF.getFunction()).first;
 
+
+  if (!RewriteMFMAToAGPR)
+    return false;
+
+  if (MFI.getMaxWavesPerEU() > 1)
+    return false;
+
   RegionsWithExcessArchVGPR.resize(DAG.Regions.size());
   RegionsWithExcessArchVGPR.reset();
   for (unsigned Region = 0; Region < DAG.Regions.size(); Region++) {
@@ -2673,12 +2680,6 @@ bool RewriteScheduleStage::rewrite(
     std::vector<std::pair<MachineInstr *, unsigned>> &RewriteCands) {
   DenseMap<MachineInstr *, unsigned> FirstMIToRegion;
   DenseMap<MachineInstr *, unsigned> LastMIToRegion;
-
-  if (!RewriteMFMAToAGPR)
-    return false;
-
-  if (MFI.getMaxWavesPerEU() > 1)
-    return false;
 
   for (unsigned Region = 0; Region < DAG.Regions.size(); Region++) {
     auto Entry = DAG.Regions[Region];
