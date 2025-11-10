34be41dbd90b462b1ddb9f7030964170cf4f615f
Jeffrey Byrnes <Jeffrey.Byrnes@amd.com>
2025-11-10 15:03:36 -0800

    Heuristic for ProcResource prioritization + Hazard
    Change-Id: I73f9e01b6fc7d476990915ecd2e9e02597763e26

--- llvm/include/llvm/CodeGen/MachineScheduler.h
+++ llvm/include/llvm/CodeGen/MachineScheduler.h
@@ -1332,6 +1332,10 @@ protected:
                          SchedCandidate &Candidate);
 
   void reschedulePhysReg(SUnit *SU, bool isTop);
+
+  unsigned getClusterID(bool isTop) const {
+    return isTop ? TopClusterID : BotClusterID;
+  }
 };
 
 /// PostGenericScheduler - Interface to the scheduling algorithm used by
