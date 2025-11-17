d0ebc65b6c20e6e7bf67fa8918c56acaf6dcc337
Austin Kerbow <Austin.Kerbow@amd.com>
2025-11-17 15:33:49 -0800

    Switch to new scaffolding
    Change-Id: I99cb303465d547559c6b4fd4c26e834471caf50a

--- llvm/lib/Target/AMDGPU/GCNSubtarget.h
+++ llvm/lib/Target/AMDGPU/GCNSubtarget.h
@@ -83,6 +83,8 @@ protected:
   bool ATTRIBUTE = DEFAULT;
 #include "AMDGPUGenSubtargetInfo.inc"
 
+  bool DisablePostMISched = false;
+
 private:
   SIInstrInfo InstrInfo;
   SITargetLowering TLInfo;
