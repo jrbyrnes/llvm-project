df9a339d006ce456ff8593f52dc49c2a2063ccda
Jeffrey Byrnes <Jeffrey.Byrnes@amd.com>
2025-11-25 07:25:46 -0800

    Try all priority resources
    Change-Id: Iead647adb299b44228b67f49eac70c2d9db1c891

--- llvm/lib/Target/AMDGPU/AMDGPUMLSchedStrategy.h
+++ llvm/lib/Target/AMDGPU/AMDGPUMLSchedStrategy.h
@@ -20,11 +20,18 @@ namespace llvm {
 class HardwareUnitInfo {
 private:
   const MCProcResourceDesc *ProcRes = nullptr;
+  // Ideally these would be sorted on how much they enable a secondary resource,
+  // but that creates a chicken and egg problem and compile time explosion.
   SmallSetVector<SUnit *, 16> PrioritySUs;
   SmallSetVector<SUnit *, 16> AllSUs;
   unsigned TotalCycles = 0;
 
 public:
+  // TODO -- handle this stuff better.
+  bool IsIgnoreable = false;
+  bool IsAsync = false;
+  unsigned Idx;
+
   HardwareUnitInfo(const MCProcResourceDesc *Res) : ProcRes(Res) {};
   HardwareUnitInfo() {}
 
@@ -32,6 +39,8 @@ public:
 
   unsigned size() { return AllSUs.size(); }
   SUnit *getTargetSU() { return *PrioritySUs.begin(); }
+
+  // TODO -- should we allow looking past the a single depth?
   SUnit *getNextTargetSU() {
     for (auto *PrioritySU : PrioritySUs) {
       if (!PrioritySU->isTopReady())
@@ -120,8 +129,8 @@ public:
 
 class AMDGPUMLSchedStrategy final : public GCNSchedStrategy {
 protected:
-  bool tryCandidate(SchedCandidate &Cand, SchedCandidate &TryCand,
-                    SchedBoundary *Zone) const override;
+  bool tryCandidateBalanced(SchedCandidate &Cand, SchedCandidate &TryCand,
+                            SchedBoundary *Zone);
 
   SmallVector<SUnit *, 16> SchedDSR;
 
@@ -131,10 +140,8 @@ protected:
 
   void collectUse();
 
-  void updateCriticalResource();
-
   bool tryPendingCandidate(SchedCandidate &Cand, SchedCandidate &TryCand,
-                           SchedBoundary *Zone) const;
+                           SchedBoundary *Zone);
 
   void pickNodeFromQueue(SchedBoundary &Zone, const CandPolicy &ZonePolicy,
                          const RegPressureTracker &RPTracker,
@@ -155,12 +162,10 @@ public:
 
   bool tryCriticalResourceDependency(SchedCandidate &TryCand,
                                      SchedCandidate &Cand, SchedBoundary *Zone,
-                                     unsigned ResourceIdx) const;
+                                     bool IsAsyncPipe = false) const;
 
   unsigned getLatencyStallCycles(SUnit *SU, unsigned CurrCycle) const;
 
-  unsigned CriticalResourceIdx;
-  unsigned SecondaryResourceIdx;
 };
 
 class AMDGPUMLPostSchedStrategy : public PostGenericScheduler {
