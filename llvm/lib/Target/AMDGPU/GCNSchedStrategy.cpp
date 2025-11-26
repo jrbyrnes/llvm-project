7b1a033d231c5ef9b5e19dda5555452a740035b6
Jeffrey Byrnes <Jeffrey.Byrnes@amd.com>
2025-11-25 16:14:47 -0800

    Revert "Try all priority resources"
    This reverts commit dc8473e58f4c3efcf0a94ef1e35b72e8bec08ed7.

--- llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
+++ llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
@@ -1155,7 +1155,6 @@ void GCNScheduleDAGMILive::runSchedStages() {
       continue;
 
     for (auto Region : Regions) {
-      // TODO -- do we really need this
       S.CollectedUse = false;
       RegionBegin = Region.first;
       RegionEnd = Region.second;
