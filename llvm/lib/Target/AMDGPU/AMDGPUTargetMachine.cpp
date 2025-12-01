9923047a16b701287ac8d2dd9ee514f40669f145
Jeffrey Byrnes <Jeffrey.Byrnes@amd.com>
2025-12-01 09:38:42 -0800

    enable by default
    Change-Id: Ibaaf5716041a17d31bb750069b287c6657480b28

--- llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
@@ -638,8 +638,9 @@ static ScheduleDAGInstrs *createSIMachineScheduler(MachineSchedContext *C) {
 }
 
 static bool isMLWorkload(const Function &F) {
-  Attribute WorkloadAttr = F.getFnAttribute("amdgpu-workload-type");
-  return WorkloadAttr.isValid() && WorkloadAttr.getValueAsString() == "ml";
+  return true;
+  //Attribute WorkloadAttr = F.getFnAttribute("amdgpu-workload-type");
+  //return WorkloadAttr.isValid() && WorkloadAttr.getValueAsString() == "ml";
 }
 
 static ScheduleDAGInstrs *
@@ -1761,7 +1762,7 @@ bool GCNPassConfig::addRegAssignAndRewriteOptimized() {
   addPass(&GCNPreRALongBranchRegID);
 
   addPass(createSGPRAllocPass(true));
-
+  addPreRewrite();
   // Commit allocated register changes. This is mostly necessary because too
   // many things rely on the use lists of the physical registers, such as the
   // verifier. This is only necessary with allocators which use LiveIntervals,
@@ -2343,6 +2344,7 @@ Error AMDGPUCodeGenPassBuilder::addRegAssignmentOptimized(
 
   addPass(RAGreedyPass({onlyAllocateSGPRs, "sgpr"}));
 
+  addPreRewrite(addPass);
   // Commit allocated register changes. This is mostly necessary because too
   // many things rely on the use lists of the physical registers, such as the
   // verifier. This is only necessary with allocators which use LiveIntervals,
