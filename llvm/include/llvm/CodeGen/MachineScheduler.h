d7b477c0d37b9d3903837534588767b4f51816f4
Jeffrey Byrnes <Jeffrey.Byrnes@amd.com>
2025-12-08 17:24:12 -0800

    Port ad hoc hazard handling to postRA

--- llvm/include/llvm/CodeGen/MachineScheduler.h
+++ llvm/include/llvm/CodeGen/MachineScheduler.h
@@ -1400,7 +1400,7 @@ public:
   }
 
 protected:
-  virtual bool tryCandidate(SchedCandidate &Cand, SchedCandidate &TryCand);
+  virtual bool tryCandidate(SchedCandidate &Cand, SchedCandidate &TryCand, SchedBoundary *Zone = nullptr);
 
   void pickNodeFromQueue(SchedBoundary &Zone, SchedCandidate &Cand);
 };
