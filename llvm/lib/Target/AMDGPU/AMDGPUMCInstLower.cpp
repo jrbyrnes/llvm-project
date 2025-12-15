963c095461c5ebf1b891a2e588386165ae6b701e
proaditya <proaditya@users.noreply.github.com>
2025-12-14 22:10:18 -0800

    Lookahead in static sim to see if some set_msb is truly exposed

--- llvm/lib/Target/AMDGPU/AMDGPUMCInstLower.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUMCInstLower.cpp
@@ -352,7 +352,8 @@ void AMDGPUAsmPrinter::emitInstruction(const MachineInstr *MI) {
     bool HasBankConflict = Info.Reason == AMDGPU::StallReason::REG_BANK;
     
     if (Info.StallCycles == 0 && !Info.InWMMAWindow && 
-        !Info.WasFused && !Info.WasExposed && !Info.IsWMMA && !HasCacheHit)
+        !Info.WasFused && !Info.WasExposed && !Info.WasMasked && 
+        !Info.IsWMMA && !HasCacheHit)
       return;
     
     std::string Comment;
@@ -370,7 +371,10 @@ void AMDGPUAsmPrinter::emitInstruction(const MachineInstr *MI) {
       OS << " Fused";
       HasContent = true;
     } else if (Info.WasExposed) {
-      OS << " Exposed";
+      if (Info.WasMasked)
+        OS << " MSB_Exposed(masked)";
+      else
+        OS << " MSB_Exposed";
       HasContent = true;
     }
     
