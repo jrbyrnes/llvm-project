202ddedcfc85268214db5afc54f298a3affa7dd9
Jeffrey Byrnes <Jeffrey.Byrnes@amd.com>
2025-12-15 16:27:15 -0800

    Consolodate

--- llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
+++ llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
@@ -1170,9 +1170,7 @@ void GCNScheduleDAGMILive::runSchedStages() {
     if (!Stage->initGCNSchedStage())
       continue;
 
-    unsigned R = 0;
     for (auto Region : Regions) {
-      errs() << "\n\n\nREgion: " << R++ << "\n";
       // TODO -- do we really need this
       S.CollectedUse = false;
       RegionBegin = Region.first;
