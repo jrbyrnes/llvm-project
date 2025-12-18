bf277d939a126cdf310af6d8bacbbdcc4c466d4a
Jeffrey Byrnes <Jeffrey.Byrnes@amd.com>
2025-12-18 12:26:42 -0800

    Use pickOnlyChoice

--- llvm/lib/CodeGen/MachineScheduler.cpp
+++ llvm/lib/CodeGen/MachineScheduler.cpp
@@ -3183,19 +3183,6 @@ SUnit *SchedBoundary::pickOnlyChoice() {
 }
 
 
-void SchedBoundary::checkAvailable() {
-  // Defer any ready instrs that now have a hazard.
-  for (ReadyQueue::iterator I = Available.begin(); I != Available.end();) {
-    if (checkHazard(*I)) {
-      Pending.push(*I);
-      I = Available.remove(I);
-      continue;
-    }
-    ++I;
-  }
-
-}
-
 #if !defined(NDEBUG) || defined(LLVM_ENABLE_DUMP)
 
 /// Dump the content of the \ref ReservedCycles vector for the
@@ -4600,12 +4587,10 @@ void PostGenericScheduler::schedNode(SUnit *SU, bool IsTopNode) {
     SU->TopReadyCycle = std::max(SU->TopReadyCycle, Top.getCurrCycle());
     TopClusterID = SU->ParentClusterIdx;
     Top.bumpNode(SU);
-    Top.checkAvailable();
   } else {
     SU->BotReadyCycle = std::max(SU->BotReadyCycle, Bot.getCurrCycle());
     BotClusterID = SU->ParentClusterIdx;
     Bot.bumpNode(SU);
-    Bot.checkAvailable();
   }
 }
 
