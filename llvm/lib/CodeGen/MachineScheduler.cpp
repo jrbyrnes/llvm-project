18dc7e62acc7f82893a162acb101a907f09a71fb
Jeffrey Byrnes <Jeffrey.Byrnes@amd.com>
2025-12-24 21:46:13 -0800

    More fine grained rules for exec slots + delete redundant WMMA physReg checks + woarkound hazardrec bumpcycle issue

--- llvm/lib/CodeGen/MachineScheduler.cpp
+++ llvm/lib/CodeGen/MachineScheduler.cpp
@@ -3110,6 +3110,10 @@ void SchedBoundary::bumpNode(SUnit *SU) {
     bumpCycle(++NextCycle);
   }
   LLVM_DEBUG(dumpScheduledState());
+
+  if (HazardRec->isEnabled()) {
+    HazardRec->RefreshState(SU);
+  }
 }
 
 /// Release pending ready nodes in to the available queue. This makes them
