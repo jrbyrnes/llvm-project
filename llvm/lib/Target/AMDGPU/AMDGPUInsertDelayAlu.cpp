39991f8fb5ad8cd8afa0b1d97bd3eb059eb5e8ef
Austin Kerbow <Austin.Kerbow@amd.com>
2026-01-11 15:44:58 -0800

    [AMDGPU] Add option to enable or disable s_delay_alu insertion
    The pass (AMDGPUInsertDelayAlu) is disabled by default after this
    patch!!
    
    TODO: Consider auto disabling this pass with ML scheduler and occupancy
    == 1 upstream

--- llvm/lib/Target/AMDGPU/AMDGPUInsertDelayAlu.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUInsertDelayAlu.cpp
@@ -21,6 +21,11 @@ using namespace llvm;
 
 #define DEBUG_TYPE "amdgpu-insert-delay-alu"
 
+static cl::opt<bool> EnableInsertDelayALU(
+    "amdgpu-enable-insert-delay-alu", cl::Hidden,
+    cl::desc("Whether or not to enable insertion of s_delay_alu instructions."),
+    cl::init(false));
+
 namespace {
 
 class AMDGPUInsertDelayAlu {
@@ -462,7 +467,7 @@ public:
     LLVM_DEBUG(dbgs() << "AMDGPUInsertDelayAlu running on " << MF.getName()
                       << "\n");
     ST = &MF.getSubtarget<GCNSubtarget>();
-    if (!ST->hasDelayAlu())
+    if (!ST->hasDelayAlu() || !EnableInsertDelayALU)
       return false;
 
     SII = ST->getInstrInfo();
