23f1696bf362524e6d6e52d595bb903c3a8b96ac
Jeffrey Byrnes <Jeffrey.Byrnes@amd.com>
2026-01-17 11:34:09 -0800

    Change default FP options

--- llvm/lib/CodeGen/CommandFlags.cpp
+++ llvm/lib/CodeGen/CommandFlags.cpp
@@ -242,7 +242,7 @@ codegen::RegisterCodeGenFlags::RegisterCodeGenFlags() {
   static cl::opt<bool> EnableNoNaNsFPMath(
       "enable-no-nans-fp-math",
       cl::desc("Enable FP math optimizations that assume no NaNs"),
-      cl::init(false));
+      cl::init(true));
   CGBINDOPT(EnableNoNaNsFPMath);
 
   static cl::opt<bool> EnableNoSignedZerosFPMath(
@@ -303,7 +303,7 @@ codegen::RegisterCodeGenFlags::RegisterCodeGenFlags() {
 
   static cl::opt<FPOpFusion::FPOpFusionMode> FuseFPOps(
       "fp-contract", cl::desc("Enable aggressive formation of fused FP ops"),
-      cl::init(FPOpFusion::Standard),
+      cl::init(FPOpFusion::Fast),
       cl::values(
           clEnumValN(FPOpFusion::Fast, "fast",
                      "Fuse FP ops whenever profitable"),
