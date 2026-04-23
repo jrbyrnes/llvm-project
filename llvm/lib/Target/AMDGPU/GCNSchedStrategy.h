836f7cb14ede5c8f27a15b28abd8f42cda9eb4a5
Ilya Chernyavsky <ichernia@amd.com>
2026-04-23 20:18:33 +0000

    [AMDGPU] Add RA pressure-aware rescheduling stage
    Machine scheduler's register pressure (RP) heuristic computes instant RP based on
    live interval segments at a given moment, which is an optimistic estimate of the number of
    registers needed. This quantity can sometimes be misleading, resulting in unnecessary
    splits or spills in RA.
    
    To address this, we introduce an RA pressure-aware scheduling stage that estimates RP
    based on live interval interference. If a significant difference between instant pressure and
    RA-like pressure is detected, we reschedule the region with a lower VGPR limit.
    
    Test:
    - llvm-lit -v llvm/test/CodeGen/AMDGPU/ra-pressure-reschedule.mir
    - perf testing on a corpus of kernels

--- llvm/lib/Target/AMDGPU/GCNSchedStrategy.h
+++ llvm/lib/Target/AMDGPU/GCNSchedStrategy.h
@@ -36,7 +36,8 @@ enum class GCNSchedStageID : unsigned {
   ClusteredLowOccupancyReschedule = 3,
   PreRARematerialize = 4,
   ILPInitialSchedule = 5,
-  MemoryClauseInitialSchedule = 6
+  MemoryClauseInitialSchedule = 6,
+  RAPressureReschedule = 7
 };
 
 #ifndef NDEBUG
@@ -90,10 +91,6 @@ protected:
 
   std::vector<unsigned> MaxPressure;
 
-  unsigned SGPRExcessLimit;
-
-  unsigned VGPRExcessLimit;
-
   unsigned TargetOccupancy;
 
   MachineFunction *MF;
@@ -131,6 +128,10 @@ public:
 
   // Bias for VGPR limits under a high register pressure.
   const unsigned HighRPVGPRBias = 7;
+
+  unsigned SGPRExcessLimit;
+
+  unsigned VGPRExcessLimit;
 
   unsigned SGPRCriticalLimit;
 
@@ -264,6 +265,7 @@ class GCNScheduleDAGMILive final : public ScheduleDAGMILive {
   friend class ClusteredLowOccStage;
   friend class PreRARematStage;
   friend class ILPInitialScheduleStage;
+  friend class RAPressureRescheduleStage;
   friend class RegionPressureMap;
 
   const GCNSubtarget &ST;
@@ -806,6 +808,21 @@ public:
   MemoryClauseInitialScheduleStage(GCNSchedStageID StageID,
                                    GCNScheduleDAGMILive &DAG)
       : GCNSchedStage(StageID, DAG) {}
+};
+
+class RAPressureRescheduleStage : public GCNSchedStage {
+public:
+  bool initGCNSchedStage() override;
+  void finalizeGCNSchedStage() override;
+  bool initGCNRegion() override;
+  bool shouldRevertScheduling(unsigned WavesAfter) override;
+
+  RAPressureRescheduleStage(GCNSchedStageID StageID, GCNScheduleDAGMILive &DAG)
+      : GCNSchedStage(StageID, DAG) {}
+
+private:
+  unsigned computeRAPressureVGPRs(unsigned RegionIdx) const;
+  unsigned SavedVGPRThresholdPercent = 0;
 };
 
 class GCNPostScheduleDAGMILive final : public ScheduleDAGMI {
